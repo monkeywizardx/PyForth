2be47df4de50f689c1d5570d3f8d5cba3f5bb182
monkeywizardx <31173746+monkeywizardx@users.noreply.github.com>
2017-08-20 12:36:32 -0500

    Removed useless exit code.
    Some minor edits to remove the non-functional exit code.

--- primitives.py
+++ primitives.py
@@ -7,10 +7,6 @@ if stack.pop() {} stack.pop():
     stack.append(1)
 else:
     stack.append(0)'''.format(operator))
-def end(exitMsg = "Bye."):
-  global executing # Need to have executing global. There's no way to avoid it, best practices or not.
-  print(exitMsg) # Print the exit message.
-  executing = False # Turn executing off, so that way we don't evaluate any more FORTH calls.
 # The collection of primitives. Mostly lambdas, but there is some calls to the functions above.
 primitives = {
   '.s': stackshow, # Show the stack.
@@ -25,6 +21,5 @@ primitives = {
   '<': lambda: forthcomp("<"),
   '>=': lambda: forthcomp('>='),
   '<=': lambda: forthcomp('<='),
-  'bye': end,
   'emit': lambda: print(chr(stack.pop())),
 }
