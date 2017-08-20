a9296348c6d7e6bb8b4755d5de1aa7d5dc80dd88
monkeywizardx <31173746+monkeywizardx@users.noreply.github.com>
2017-08-20 12:54:25 -0500

    Update forth.py
    Fixed some spacing issues. Curse you github editor!

--- forth.py
+++ forth.py
@@ -27,9 +27,9 @@ def forth_eval(parsed_array):
               word_dict[function_name] = code_str
               line_ptr += 1
             elif word == "bye":
-		print("Bye.")
-		executing = False
-	    elif word == "@":
+                print("Bye.")
+                executing = False
+            elif word == "@":
               line_ptr += 1 # Force the pointer ahead so the variable isn't treated as code.
               stack.append(variables[parsed_array[line_ptr]]) # Append the value of the variable.
             elif word == "!": # This is the "setter" command for the variable.
