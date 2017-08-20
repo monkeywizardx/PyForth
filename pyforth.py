c89cdabb409d90c39a83540ce1768b3aa35d6f6e
monkeywizardx <31173746+monkeywizardx@users.noreply.github.com>
2017-08-20 13:15:41 -0500

    Update pyforth.py

--- pyforth.py
+++ pyforth.py
@@ -8,7 +8,7 @@ def main_loop():
     try: run(input("pyforth>"))
     except IndexError:
       print("Stack Error!")
-      stack.append(-127)
+      forth.stack.append(-127)
 
 if __name__ == "__main__":
   if len(argv) == 1:
