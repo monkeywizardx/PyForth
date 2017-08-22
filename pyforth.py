784e2191bc8a6e52c34a96ca6faa363428098cec
monkeywizardx <31173746+monkeywizardx@users.noreply.github.com>
2017-08-21 20:50:34 -0500

    Began rewriting REPL code.
    Began to rewrite the REPL. Until FORTH-eval is finished, it's not a priority.

--- pyforth.py
+++ pyforth.py
@@ -1,19 +1,10 @@
-from sys import argv
 import forth
+executing = true
+PyForth = forth.Forth()
+def parse(string):
+	return string.lower().split()
 
-def run(string):
-	forth.forth_eval(forth.parse(string))
-def main_loop():
-  while forth.executing:
-    try: run(input("pyforth>"))
-    except IndexError:
-      print("Stack Error!")
-      forth.stack.append(-127)
-
-if __name__ == "__main__":
-  if len(argv) == 1:
-    main_loop()
-  else:
-    file = open(argv[1], 'r')
-    run(file.read())
-    main_loop()
+while executing:
+	code = parse(input("forth>"))
+	if "bye" in code:
+		executing = false
