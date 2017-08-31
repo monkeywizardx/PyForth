d103eeb660118f9fe3c97856aaf3bdf49cd2ccc1
monkeywizardx <31173746+monkeywizardx@users.noreply.github.com>
2017-08-31 16:49:49 +0000

    Added the newest version of forth.py
    I rewrote the forth.py from scratch for the fourth time, adding in a simple parser. It could still be further divided to meet best practices, but I did't have enough time. I still have to rewrite the REPL from scratch to make it work. It also only includes primitives as of right now, but that's easy enough to fix.

--- forth.py
+++ forth.py
@@ -1,115 +1,128 @@
-class Stack: 
-  '''This is a simple wrapper class for creating a stack. Since the stack simply needs to have items popped and pushed, it's easier
-  to have it written this way.'''
-  def __init__(self):
-    self.stack = []
-  def push(self, value):
-    self.stack.append(value)
-  def pop(self):
-    return self.stack.pop()
-  def getStack(self):
-    return self.stack
+class Stack:
+    '''
+    Stack is a wrapper class for arrays to prevent arbitrary access.
+    It's interacted with through the push, pop, and peek commands.
+    These should be the only things necessary to use the stack.
+    '''
+    def __init__(self):
+        self.stack = []
+    def __repr__(self):
+      return '<{}> {}'.format(len(self.stack), ' '.join(map(str, self.stack)))
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        return self.stack.pop()
+    def peek(self):
+        return self.stack[-1]
 
+def setVariable(forth):
+  forth.variables[forth.current_variable] = forth.stack.pop()
+def getVariable(forth):
+  forth.stack.push(forth.variables[forth.current_variable])
 class Forth:
-  '''This is the VM that executes all PyFORTH code in turns of primitives and words.'''
-  # -----Boilerplate Required By Python----
-  def __init__(self, words):
-    self.memory = []
-    self.pointer = 0 
-    self.primitives = {
-      '+': lambda: self.stack.push(self.stack.pop() + self.stack.pop()),
-      '*': lambda: self.stack.push(self.stack.pop() * self.stack.pop()),
-      '/': lambda: self.stack.push(self.stack.pop() // self.stack.pop()),
-      'mod': lambda: self.stack.push(self.stack.pop() ** self.stack.pop()),
-      '^': lambda: self.stack.push(self.stack.pop() % self.stack.pop()),
-      '.': lambda: print(self.stack.pop()),
-      '>': lambda: self.stack.push(-1) if self.stack.pop() > self.stack.pop() else self.stack.push(0),
-      '=': lambda: self.stack.push(-1) if self.stack.pop() == self.stack.pop() else self.stack.push(0),
-      '<': lambda: self.stack.push(-1) if self.stack.pop() < self.stack.pop() else self.stack.push(0),
-      '<=': lambda: self.stack.push(-1) if self.stack.pop() <= self.stack.pop() else self.stack.push(0),
-      '>=': lambda: self.stack.push(-1) if self.stack.pop() >= self.stack.pop() else self.stack.push(0),
-      'bye': lambda: 0,
-    } 
-    # ^ Create the primitives array. All primitives are pieces of Python code, usually wrapped in a lambda.
-    self.words = words # This is the beginning definition of words.
-    self.stack = Stack()
-    self.variables = {}
-  # -----Memory Manipulation-----
-  def write_to_memory(self, value, location):
-    self.memory[location] = value
+    '''
+    Forth is a simple class meant to encapsulate all PyForth related code and prevent it from spilling over into the main namespace.
+    '''
+    def __init__(self, words):
+        self.stack = Stack()
+        self.primitives = {
+            '+': lambda self: self.stack.push(self.stack.pop() + self.stack.pop()),
+            '*': lambda self: self.stack.push(self.stack.pop() * self.stack.pop()),
+            '-': lambda self: self.stack.push(self.stack.pop() - self.stack.pop()),
+            '/': lambda self: self.stack.push(self.stack.pop() // self.stack.pop()),
+            'MOD': lambda self: self.stack.push(self.stack.pop() % self.stack.pop()),
+            '.': lambda self: print(self.stack.pop()),
+            '.s': lambda self: print(self.stack),
+            'DROP': lambda self: self.stack.pop(),
+            '!': setVariable, # This is a non-class function. Curse you kludges!
+            '@': getVariable,
+            }
+        self.variables = {'NOP': 0}
+        self.current_variable = 'NOP'
+        self.words = words
+    def parse(self, string):
+        return self.tokenize(string.split())
+    def tokenize(self, input_stream):
+        '''
+          Tokenize takes an array and pops through it, finding the type and value of the object. It violates branching guidelines, but it's also the best way I could think of. If some brave citizen would help, I would be eternally thankful.
+        '''
+        tokens = []
+        while input_stream != []:
+          token = input_stream.pop(0)
+          token_form = None
+          try: self.words[token]
+          except KeyError:
+            pass
+          else:
+            token_form = ("WORD", token)
+          try: self.primitives[token]
+          except KeyError:
+            pass
+          else:
+            if token_form == None:
+              token_form = ("PRIMITIVE", token)
+          try: int(token)
+          except ValueError:
+            pass
+          else:
+            if token_form == None:
+              token_form = ("NUMBER", int(token))
+          try: self.variables[token]
+          except KeyError:
+            pass
+          else:
+            if token_form == None:
+              token_form = ("VARIABLE", token)
+          if token == ':' and token_form == None:
+            wordDefinition = []
+            while input_stream[0] != ';':
+              wordDefinition.append(input_stream.pop(0))
+            input_stream.pop()
+            token_form = ("WORD_DEF", wordDefinition.pop(0), ' '.join(wordDefinition))
+          if token == 'IF' and token_form == None:
+            ifDefinition = []
+            elseDefinition = []
+            while input_stream[0].upper() != 'ELSE':
+              ifDefinition.append(input_stream.pop(0))
+            input_stream.pop(0)
+            while input_stream[0].upper() != 'THEN':
+              elseDefinition.append(input_stream.pop(0))
+            input_stream.pop(0)
+            token_form = ("IF", ' '.join(ifDefinition), ' '.join(elseDefinition))
+          if token_form == None and token == 'VARIABLE':
+            token_form = ('VARIABLE_CREATE', input_stream.pop(0))
+          if token_form == None:
+            token_form = ("UNKNOWN", token)
+          tokens.append(token_form)
+        return tokens
+    def evaluate(self, token_stream):
+      while token_stream != []:
+        token = token_stream.pop(0)
+        token_type = token[0]
+        if token_type == "WORD":
+          word_stream = self.parse(self.words[token[1]])
+          while word_stream != []:
+            token_stream.insert(0, word_stream.pop())
+        if token_type == "PRIMITIVE":
+          self.primitives[token[1]](self)
+        if token_type == "NUMBER":
+          self.stack.push(token[1])
+        if token_type == "WORD_DEF":
+          self.words[token[1]] = token[2]
+        if token_type == 'VARIABLE_CREATE':
+          self.variables[token[1]] = 0
+        if token_type == "IF":
+          truth_value = self.stack.pop()
+          if truth_value == 0:
+            word_stream = self.parse(self.words[token[2]])
+            while word_stream != []:
+              token_stream.insert(0, word_stream.pop())
+          else:
+            word_stream = self.parse(self.words[token[1]])
+            while word_stream != []:
+              token_stream.insert(0, word_stream.pop())
+        if token_type == "VARIABLE":
+          self.current_variable = token[1]
+        if token_type == "UNKNOWN":
+          print("Unknown word at {}. Skipping.".format(token[1]))
   
-  def get_memory_at_pointer(self):
-    return self.memory[self.pointer]
-  # -----Word Manipulation-----
-  def get_word(self, word):
-    try: self.words[word]
-    except KeyError:
-      try: self.primitives[word]
-      except KeyError:
-        return ""
-      else:
-        return "primitive"
-    else:
-      return "word"
-  
-  def addWord(self):
-    '''self.pointer += 1
-    word_name = self.get_memory_at_pointer()
-    code_str = ""
-    self.pointer += 1
-    while self.get_memory_at_pointer() != ';':
-      if self.get_memory_at_pointer() == word_name:
-        print("Error! Undefined word at {}! Excising word.".format(word_name))
-      else:
-        code_str += self.get_memory_at_pointer() + " "
-      self.pointer += 1
-    self.words[word_name] = code_str'''
-  # -----Macro          Code-----
-  def ifStatement(self):
-    pass
-  
-  def loop(self):
-    pass
-  # -----Variable Manipulation-----
-  def setVariable(self):
-    self.pointer += 1
-    try: variable_name = self.get_memory_at_pointer()
-    except IndexError:
-      print("VariableError: No variable name!")
-    try: self.variables[variable_name] = self.stack.pop()
-    except IndexError:
-      print("StackError: Empty stack!")
-  def getVariable(self):
-    self.pointer += 1
-    self.stack.push(self.variables[self.get_memory_at_pointer()])
-  # -----Code Interpretation-----
-  def load(self, parsed_array):
-    for i in parsed_array:
-     self.memory.insert(len(self.memory), i)
-  def eval(self):
-    while self.pointer <= len(self.memory) - 1:
-      item = self.get_memory_at_pointer()
-      itemType = self.get_word(item)
-      if itemType == "word":
-        self.memory.insert(self.pointer + 1, self.words[item])
-      elif itemType == "primitive":
-        self.primitives[item]()
-      elif itemType == "":
-        try: int(item)
-        except ValueError:
-          itemType = "macro"
-        else:
-          self.stack.push(int(item))
-      if itemType == "macro":
-        if item == ":":
-          self.addWord()
-        elif item == "if":
-          self.ifStatement()
-        elif item == "do":
-          self.loop()
-        elif item == "!":
-          self.setVariable()
-        elif item == "@":
-          self.getVariable()
-      self.pointer += 1
-      
